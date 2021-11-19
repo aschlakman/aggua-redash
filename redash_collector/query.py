ffd1027d23264d717a04f3629092058bd32521ca
Ari <arischlakman@gmail.com>
2021-11-19 11:51:16 +0200

    Added dashboard model

--- redash_collector/query.py
+++ redash_collector/query.py
@@ -2,7 +2,7 @@ from typing import Optional, Dict
 from pydantic import BaseModel
 
 
-class Query(BaseModel):
+class QueryModel(BaseModel):
     id: int
     data_source_id: int
     name: str
@@ -13,10 +13,10 @@ class Query(BaseModel):
 
     @staticmethod
     def from_json(data: Dict):
-        return Query(id=data['id'],
-                     data_source_id=data['data_source_id'],
-                     name=data['name'],
-                     description=data['description'],
-                     sql=data['query'],
-                     owner_name=data['user'].get('name'),
-                     last_update_time=data['retrieved_at'])
+        return QueryModel(id=data['id'],
+                          data_source_id=data['data_source_id'],
+                          name=data['name'],
+                          description=data['description'],
+                          sql=data['query'],
+                          owner_name=data['user'].get('name'),
+                          last_update_time=data['retrieved_at'])
